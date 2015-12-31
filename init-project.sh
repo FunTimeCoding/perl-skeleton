ac163b9664dd281b2a2b708e54ea6da46779a992
Alexander Reitzel <funtimecoding@gmail.com>
2015-12-31 16:38:58 +0100

    add an init project script

---
+++ init-project.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -e
+# This tool can be used to initialise the template after making a fresh copy to get started quickly.
+# The goal is to make it as easy as possible to create scripts that allow easy testing and continuous integration.
+
+CAMEL=$(echo "${1}" | grep -E '^([A-Z][a-z0-9]+){2,}$') || CAMEL=""
+
+if [ "${CAMEL}" = "" ]; then
+    echo "Usage: ${0} UpperCamelCaseName"
+
+    exit 1
+fi
+
+OPERATING_SYSTEM=$(uname)
+
+if [ "${OPERATING_SYSTEM}" = "Darwin" ]; then
+    SED="gsed"
+else
+    SED="sed"
+fi
+
+UNDERSCORE=$(echo "${CAMEL}" | ${SED} -E 's/([A-Za-z0-9])([A-Z])/\1_\2/g' | tr '[:upper:]' '[:lower:]')
+INITIALS=$(echo "${CAMEL}" | ${SED} 's/\([A-Z]\)[a-z]*/\1/g' | tr '[:upper:]' '[:lower:]')
+echo "INITIALS: ${INITIALS}"
+echo "UNDERSCORE: ${UNDERSCORE}"
+find -E . -type f ! -regex '^.*/(build|\.git|\.idea)/.*$' -exec sh -c '${1} -i -e "s/PerlSkeleton/${2}/g" -e "s/perl_skeleton/${4}/g" -e "s/bin\/ps/bin\/${5}/g" ${6}' '_' "${SED}" "${CAMEL}" "${UNDERSCORE}" "${INITIALS}" '{}' \;
+git mv test/test_perl_skeleton.pl "test/test_${UNDERSCORE}.pl"
+git mv lib/perl_skeleton.pm "lib/${UNDERSCORE}.pm"
+git mv lib "${UNDERSCORE}"
+git mv bin/ps "bin/${INITIALS}"
+rm init-project.sh sync-project.sh
+echo "Done. Files were edited and moved using git. Review those changes."
--- run-tests.sh
+++ run-tests.sh
@@ -2,7 +2,7 @@
 
 if [ "${1}" = "--ci-mode" ]; then
     mkdir -p build/log
-    perl test/*_test.pl | tee build/log/test-output.txt
+    perl test/test_*.pl | tee build/log/test-output.txt
 else
-    perl test/*_test.pl
+    perl test/test_*.pl
 fi
